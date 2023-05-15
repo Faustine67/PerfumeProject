5af7ddcbc4e6e7274d200d93f8c8564839503f91
Faustine67 <michelfaustine@gmail.com>
2023-05-15 16:20:40 +0200

    add index + detail marque

--- src/Controller/MarqueController.php
+++ src/Controller/MarqueController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Marque;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class MarqueController extends AbstractController
 {
+    //Afficher toutes les marques
     #[Route('/marque', name: 'app_marque')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MarqueController.php',
+        public function index(ManagerRegistry $doctrine): Response
+        {
+        $marques= $doctrine->getRepository(Marque::class)->findAll();
+        return $this->render('marque/index.html.twig', [
+            'marques' => $marques,
         ]);
-    }
+        }
+
+    //Afficher les marques d'une marque
+    #[Route('/marque/{id}', name: 'detail_marque')]
+        public function detail (ManagerRegistry $doctrine, $id): Response
+        {
+            $marque= $doctrine->getRepository(Marque::class)->find($id);
+           
+            if (!$marque) {
+                throw $this->createNotFoundException('La marque n\'existe pas.');
+            }
+            return $this->render('marque/detail.html.twig', [
+                'marque' => $marque,]);
+        }
 }
