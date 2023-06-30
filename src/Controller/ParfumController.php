6b2bcb2a83fd3cb4970fcca51ff983325d247509
Faustine67 <michelfaustine@gmail.com>
2023-06-30 14:20:53 +0200

    commentaire + profil

--- src/Controller/ParfumController.php
+++ src/Controller/ParfumController.php
@@ -5,9 +5,12 @@ namespace App\Controller;
 use App\Entity\Marque;
 use App\Entity\Parfum;
 use App\Form\DupeType;
+use DateTimeImmutable;
 use App\Form\ParfumType;
 use App\Form\SearchForm;
 use App\Model\SearchData;
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
 use App\Repository\ParfumRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
@@ -87,13 +90,38 @@ class ParfumController extends AbstractController
 
     
     //Afficher un parfum par Id
-    #[Route('/parfum/{id}', name: 'detail_parfum')]
-        public function detail (ManagerRegistry $doctrine, $id, Parfum $parfums): Response
+    #[Route('/parfum/{id}', name: 'detail_parfum', methods:['GET', 'POST'])]
+        public function detail (EntityManagerInterface $entityManager,ManagerRegistry $doctrine, $id, Parfum $parfums, Request $request): Response
         {
             $parfum= $doctrine->getRepository(Parfum::class)->find($id);
            
             if (!$parfum) {
                 throw $this->createNotFoundException('Le parfum n\'existe pas.');
+            }
+
+            {
+                // Créer une instance de Commentaire
+                $commentaire = new Commentaire();
+                $commentaire->setIsApprouved(false);
+                $commentaire->setParfum($parfum);
+                if($this->getUser()) {
+                    $commentaire->setUser($this->getUser());
+                }
+            }
+            
+            // Créer le formulaire pour ajouter un commentaire
+            $form= $this->createForm (CommentaireType::class, $commentaire);
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()) {
+                //Recuperer la date actuelle
+                $dateActuelle = new DateTimeImmutable();
+                $commentaire->setDateCommentaire($dateActuelle);
+
+                //Enregistrer le commentaire dans la base de données
+                $entityManager->persist($commentaire);
+                $entityManager->flush();
+                $this->addFlash('success', 'Votre commentaire a bien été enregistré. Il sera soumis à modération dans les plus brefs délais.');
             }
 
             $marque = $parfum->getMarque();
@@ -102,6 +130,7 @@ class ParfumController extends AbstractController
                 'parfum' => $parfums,
                 'marque'=>$marque,
                 'dupes'=>$dupes,
+                'form'=>$form->createView(),
             ]);
         }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/user/{id}', name: 'profil_user', methods:['GET', 'POST'])]
+    public function profil(EntityManagerInterface $entityManager, ManagerRegistry $doctrine, $id, User $user, Request $request): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('L\'Utilisateur n\' existe pas.');
+        }
+
+        $prenom = $user->getPrenom();
+        $nom = $user->getNom();
+        $email = $user->getEmail();
+
+        return $this->render('user/profil.html.twig', [
+            'user' => $user,
+            'prenom' => $prenom,
+            'nom' => $nom,
+            'email' => $email,
         ]);
     }
 }
+
+
